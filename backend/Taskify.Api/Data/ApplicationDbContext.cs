4e012b9fe440066fc14b8e5eef350eb7ff306a15
Uzair Iqbal <uzair.qq@outlook.com>
2026-07-15 02:42:26 +0500

    feat: add TaskItem model and ApplicationDbContext

---
+++ backend/Taskify.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taskify.Api.Models;
+
+namespace Taskify.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
