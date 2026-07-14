424a8c3f86f7582c6993c118856b39de5efbb4b1
Uzair Iqbal <uzair.qq@outlook.com>
2026-07-14 14:08:44 +0500

    feat: add database migrations and update Program.cs with swagger

--- backend/Taskify.Api/Program.cs
+++ backend/Taskify.Api/Program.cs
@@ -22,12 +22,16 @@ namespace Taskify.Api
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI();
                 app.MapOpenApi();
             }
 
