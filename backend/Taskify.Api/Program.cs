12851d09901a6dbf3a13406263d9bad096ddd75b
Uzair Iqbal <uzair.qq@outlook.com>
2026-07-15 17:25:36 +0500

    fix: Swagger version for .NET 9

--- backend/Taskify.Api/Program.cs
+++ backend/Taskify.Api/Program.cs
@@ -22,15 +22,15 @@ namespace Taskify.Api
             builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
